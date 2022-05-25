1e93afbb5fa4c3608609a84e1eecdef8fff2a56d
Hollis <hollis@droid.rocks>
2022-05-24 20:44:48 -0400

    Initial commit

---
+++ app/src/main/java/hollis/inman/polydemo/MainActivity.kt
@@ -0,0 +1,18 @@
+package hollis.inman.polydemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import hollis.inman.polydemo.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
